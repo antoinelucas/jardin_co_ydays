ffc9013531ecdc83da14f81816682108f1db82ac
Antoine LUCAS <antoine.lucas@ynov.com>
2017-02-08 12:04:12 +0100

    commit

---
+++ creation_json.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoine Lucas
+ * Date: 08/02/2017
+ * Time: 10:07
+ */
+
+$a = array(array(array('idUtilisateur' => 2,'login' => "alex" )));
+
+echo "Tableau associatif affiché comme objet: ", json_encode($a, JSON_FORCE_OBJECT), "\n\n";
+
+
+/*  $contenu_json =json_encode($creation_json);
+
+// Nom du fichier à créer
+$nom_du_fichier = 'fichier.json';
+
+// Ouverture du fichier
+$fichier = fopen($nom_du_fichier, 'w+');
+
+// Ecriture dans le fichier
+fwrite($fichier, $creation_json);
+
+// Fermeture du fichier
+fclose($fichier);
--- testAntoine.php
+++ testAntoine.php
@@ -10,12 +10,14 @@
 
 $json = file_get_contents("http://www.serveur.nom_du_json.json");
 
-var_dump(json_decode($json)); // présente notre fichier json sous la forme d'objets (chaque objet peut lui même contenir un objet ou un tableau par exemple)
+ // présente notre fichier json sous la forme d'objets (chaque objet peut lui même contenir un objet ou un tableau par exemple)
 
 $parsed_json = json_decode($json);
+var_dump($parsed_json);
+
 
 // on retourne la date dans la variable $date_jour en passant dans tout les objets du json jusqu'à la date.
-$date_jour = $parsed_json ->{'response'}->{'features'}->{'date'};
+
 
 
 
