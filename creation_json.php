dd22e848ec06c246ae55403f347cef4a9a2fa136
Antoine LUCAS <antoine.lucas@ynov.com>
2017-02-08 16:23:37 +0100

    commit

--- creation_json.php
+++ creation_json.php
@@ -8,7 +8,7 @@
 
 header('Content-Type: text/html; charset=utf-8');
 
-/*
+
 $a = array ("jardin_co" => array("utilisateurs" => array("idUtilisateur" => "2","login" => "alex" )));
 
 echo "Tableau associatif affiche comme objet: ", json_encode($a, JSON_FORCE_OBJECT), "\n\n";
@@ -16,30 +16,26 @@ $b = utf8_encode(json_encode($a, JSON_FORCE_OBJECT));
 echo "\n\n";
 var_dump($b);
 
-*/
 
-$b = '{"jardin_co":{"utilisateurs":{"idUtilisateur":"2","login":"alex"}}}';
 
-/*    // Définition des erreurs
-$constants = get_defined_constants(true);
-$json_errors = array();
-foreach ($constants["json"] as $name => $value) {
-    if (!strncmp($name, "JSON_ERROR_", 11)) {
-        $json_errors[$value] = $name;
-    }
-}
 
-// Affichage des erreurs pour les différentes profondeurs.
-foreach (range(4, 3, -1) as $depth) {
-    var_dump(json_decode($b, true, $depth));
-    echo 'Dernière erreur : ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
-}
+// Nom du fichier à créer
+$nom_du_fichier = 'fichier.json';
 
-*/
+// Ouverture du fichier
+$fichier = fopen($nom_du_fichier, 'w+');
+
+// Ecriture dans le fichier
+fwrite($fichier, $b);
+
+// Fermeture du fichier
+fclose($fichier);
+
+$json = file_get_contents("http://antoine-lucas.ynov-nantes.net/fichier.json");
 
 // -------------------------------------------TEST---------------------------------------
 
-$parsed_json = utf8_encode(json_decode($b));
+$parsed_json = json_decode($b);
 
 var_dump($parsed_json);
 
@@ -49,20 +45,3 @@ $user_now = $parsed_json ->{"jardin_co"}->{"utilisateur"}->{"idUtilisateus"};
 $user_login = $parsed_json ->{"jardin_co"}->{"utilisateur"}->{"login"};
 
 echo " L'id de l'utilisateur actuel : ".$user_now."\t Son login : ".$user_login;
-
-
-
-
-/*  $contenu_json =json_encode($creation_json);
-
-// Nom du fichier à créer
-$nom_du_fichier = 'fichier.json';
-
-// Ouverture du fichier
-$fichier = fopen($nom_du_fichier, 'w+');
-
-// Ecriture dans le fichier
-fwrite($fichier, $creation_json);
-
-// Fermeture du fichier
-fclose($fichier);
