e7dd579ba8c54184a4b87d73576b3b4ecf5f7f7e
Antoine LUCAS <antoine.lucas@ynov.com>
2017-02-08 12:17:08 +0100

    commit

--- testAntoine.php
+++ testAntoine.php
@@ -8,13 +8,15 @@
 
 //indique le chemin du fichier json : une URL
 
-$json = file_get_contents("http://www.serveur.nom_du_json.json");
+$json = file_get_contents("http://antoine-lucas.ynov-nantes.net/exemple_json_jardin_co");
 
  // présente notre fichier json sous la forme d'objets (chaque objet peut lui même contenir un objet ou un tableau par exemple)
 
 $parsed_json = json_decode($json);
 var_dump($parsed_json);
 
+
+include_once 'recup_objet_json.php';
 
 // on retourne la date dans la variable $date_jour en passant dans tout les objets du json jusqu'à la date.
 
